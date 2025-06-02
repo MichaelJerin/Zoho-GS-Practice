a0d4054a24d194cd34e6ed07ee9987624051550c
MichaelJerin <145703067+MichaelJerin@users.noreply.github.com>
2025-06-02 12:54:10 +0530

    Update Question_05.java

--- Question_05.java
+++ Question_05.java
@@ -6,22 +6,27 @@ public class Question_05 {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the n value: ");
         int n = in.nextInt();
-        System.out.println("Addition");
-        System.out.println(Arrays.toString(Addition(n)));
-        System.out.print("Division");
-        System.out.println(Arrays.toString(Division(n)));
+        System.out.println("Addition Table : ");
+//        System.out.println(Arrays.toString(Addition(n)));
+        Addition(n);
+        System.out.println();
+        System.out.println("Division Table : ");
+//        System.out.println(Arrays.toString(Division(n)));
+        Division(n);
     }
     public static int[] Addition(int n){
-        int[] add = new int[10];
-        for (int i = 0; i < 10; i++) {
+        int[] add = new int[11];
+        for (int i = 1; i <= 10; i++) {
             add[i] = i + n;
+            System.out.println(i + " + " + n + " = " + add[i]);
         }
         return add;
     }
     public static int[] Division(int n){
-        int[] div = new int[10];
-        for (int i = 0; i < 10; i++) {
+        int[] div = new int[11];
+        for (int i = 1; i <= 10; i++) {
             div[i] = i / n;
+            System.out.println(i + " / " + n + " = " + div[i]);
         }
         return div;
     }
